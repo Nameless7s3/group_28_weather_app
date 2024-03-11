ec26864607f95db97aad21024b7fc6f9477654a7
lukel <lukeliew9@gmail.com>
2024-03-11 10:59:39 +0000

    fixed some css

--- src/Pages/WeatherSetup.js
+++ src/Pages/WeatherSetup.js
@@ -47,65 +47,64 @@ function WeatherSetup() {
     <body className={styles.body}>
       <div className={styles.container}>
 
-        <div class="box1">
+        <div className={styles.box1}>
           <h1>Timings Setter</h1>
-          <h1 class="time">Start Time:</h1>
-          <h1 class="time" id = "end">End Time:</h1>
+          <h1 className={styles.time}>Start Time:</h1>
+          <h1 className={styles.time} id = "end">End Time:</h1>
         </div>
 
-        <div class="SliderWrap">
+        <div className={styles.SliderWrap}>
           <input type="range" min="0" max="26" value="0" step="1" class="slider" id="slider"></input>
         </div>
        
 
-        <div className={styles.long} id="one">.</div>
-        <div class="long" id="seven">.</div>
-        <div class="long" id="thirteen">.</div>
-        <div class="long" id="nineteen">.</div>
-        <div class="long" id="twentyfive">.</div>
+        <div className={styles.long} id={styles.one}></div>
+        <div className={styles.long} id={styles.seven}></div>
+        <div className={styles.long} id={styles.nineteen}>.</div>
+        <div className={styles.long} id={styles.twentyfive}>.</div>
 
-        <div class="short" id="two">.</div>
-        <div class="short" id="three">.</div>
-        <div class="short" id="four">.</div>
-        <div class="short" id="five">.</div>
-        <div class="short" id="six">.</div>
-        <div class="short" id="eight">.</div>
-        <div class="short" id="nine">.</div>
-        <div class="short" id="ten">.</div>
-        <div class="short" id="eleven">.</div>
-        <div class="short" id="twelve">.</div>
-        <div class="short" id="fourteen">.</div>
-        <div class="short" id="fifteen">.</div>
-        <div class="short" id="sixteen">.</div>
-        <div class="short" id="seventeen">.</div>
-        <div class="short" id="eighteen">.</div>
-        <div class="short" id="twenty">.</div>
-        <div class="short" id="twentyone">.</div>
-        <div class="short" id="twentytwo">.</div>
-        <div class="short" id="twentythree">.</div>
-        <div class="short" id="twentyfour">.</div>
+        <div className={styles.short} id={styles.two}>.</div>
+        <div className={styles.short} id={styles.three}>.</div>
+        <div className={styles.short} id={styles.four}>.</div>
+        <div className={styles.short} id={styles.five}>.</div>
+        <div className={styles.short} id={styles.six}>.</div>
+        <div className={styles.short} id={styles.eight}>.</div>
+        <div className={styles.short} id={styles.nine}>.</div>
+        <div className={styles.short} id={styles.ten}>.</div>
+        <div className={styles.short} id={styles.eleven}>.</div>
+        <div className={styles.short} id={styles.twelve}>.</div>
+        <div className={styles.short} id={styles.forteen}>.</div>
+        <div className={styles.short} id={styles.fifteen}>.</div>
+        <div className={styles.short} id={styles.sixteen}>.</div>
+        <div className={styles.short} id={styles.seventeen}>.</div>
+        <div className={styles.short} id={styles.eighteen}>.</div>
+        <div className={styles.short} id={styles.twenty}>.</div>
+        <div className={styles.short} id={styles.twentyone}>.</div>
+        <div className={styles.short} id={styles.twentytwo}>.</div>
+        <div className={styles.short} id={styles.twentythree}>.</div>
+        <div className={styles.short} id={styles.twentyfour}>.</div>
 
-        <h1 class="smallTime">12</h1>
-        <h1 class="smallTime" id="sixAM">6</h1>
-        <h1 class="smallTime" id="twelveAM">12</h1>
-        <h1 class="smallTime" id="sixPM">6</h1>
-        <h1 class="smallTime" id = "twelvePM">12</h1>
+        <h1 className={styles.smallTime}>12</h1>
+        <h1 className={styles.smallTime} id={styles.sixAM}>6</h1>
+        <h1 className={styles.smallTime} id={styles.twelveAM}>12</h1>
+        <h1 className={styles.smallTime} id={styles.sixPM}>6</h1>
+        <h1 className={styles.smallTime} id = {styles.twelvePM}>12</h1>
 
-        <h1 class="smalltext">am</h1>
-        <h1 class="smalltext" id="am1">am</h1>
-        <h1 class="smalltext" id="pm1">pm</h1>
-        <h1 class="smalltext" id="pm2">pm</h1>
-        <h1 class="smalltext" id="am2">am</h1>
+        <h1 className={styles.smallText}>am</h1>
+        <h1 className={styles.smallText} id="am1">am</h1>
+        <h1 className={styles.smallText} id="pm1">pm</h1>
+        <h1 className={styles.smallText} id="pm2">pm</h1>
+        <h1 className={styles.smallText} id="am2">am</h1>
 
 
-        <div class="box2">
+        <div className={styles.box2}>
           <h1>Campus Finder</h1>
         </div>
 
         
-        <form id="form" role="search">
-          <input id = "searching" type="search" name="q" placeholder="Search Campuses..." aria-label="Search through site content"></input>
-          <button><svg viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg></button>
+        <form className={styles.form} id="form" role="search">
+          <input className={styles.searchInput} id = "searching" type="search" name="q" placeholder="Search Campuses..." aria-label="Search through site content"></input>
+          <button className={styles.button}><svg viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg></button>
         </form>
         
         {selectedPlaces.length > 0 && (
