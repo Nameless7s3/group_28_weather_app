122940e3312f6f46811faa155ad69167bf2af02e
lukel <lukeliew9@gmail.com>
2024-03-18 11:23:49 +0000

    removed geolocating and replaced

--- src/Components/Weather_Data/WeatherData.js
+++ src/Components/Weather_Data/WeatherData.js
@@ -5,21 +5,23 @@ import { json } from "react-router-dom";
 export function WeatherData(){
     //const apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=${city},{state code},{country code}&limit={limit}&appid=30c05f2feb3b0253ed29f27de25f7585';
 
-    console.log("sdfogiknhjhldsifgkbjhlsi;dbugj")
-
-    const [coords, setCoords] = useState("")
-    const city = 'Fulham,London'
-    const limit = '1'
-
-    const coordsApiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit='+limit+'&appid=30c05f2feb3b0253ed29f27de25f7585';
+    const [weather, setWeather] = useState("")
+    const city = 'London, Mile End, GB'
+    
+    const weatherAtCityApiUrl = 'http://api.openweathermap.org/data/2.5/forecast?q='+city+'&units=metric&mode=json&appid=30c05f2feb3b0253ed29f27de25f7585'
 
     useEffect(() => {
         const fetchData = async () => {
-            const result = await fetch(coordsApiUrl)
+            const result = await fetch(weatherAtCityApiUrl)
+
             result.json().then(json => {
-                console.log(json);
+                setWeather(json)
+                console.log(json)
             })
         }
         fetchData();
     }, []);
+
+    return 
 }
+
