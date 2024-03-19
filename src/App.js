b33674d01d3ca5d3c672ea1a2b023f664b5f1afa
Haidar Mohsen <h.mohsen@se22.qmul.ac.uk>
2024-03-19 17:04:44 +0000

    Adding path to daybuttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import WeatherPage from "./Pages/weather_page";
 import WeatherSetup from "./Pages/WeatherSetup";
+import WeatherDays from "./Pages/DayButtons";
 //
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         {/* This does the routing to all the different pages */}
         <Route path="/" element={<WeatherSetup/>}/>
         <Route path="/weather_page" element={<WeatherPage/>}/>
+        <Route path="/weather_days" element={<WeatherDays/>}/>
       </Routes>
     </HashRouter>
   );
