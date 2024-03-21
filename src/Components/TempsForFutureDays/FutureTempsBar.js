8a9edbf95541943de3ae0b592340847c12436f4e
Luke Liew Mouawad <ec22711@qmul.ac.uk>
2024-03-21 01:17:39 +0000

    weather page now displays future chosen day's weather.

--- src/Components/TempsForFutureDays/FutureTempsBar.js
+++ src/Components/TempsForFutureDays/FutureTempsBar.js
@@ -1,29 +1,6 @@
-import TempForFutureDay from "./TempForFutureDay";
-import {WeatherData} from "../Weather_Data/WeatherData";
-import styles from "./FutureTempsBar.module.css";
-
-// This is the data for the future temperatures
-const weatherData ={
-  day01Name : "Monday",
-  day01Temp : "11°C",
-  day02Name : "Tuesday",
-  day02Temp : "9°C",
-  day03Name : "Wednesday",
-  day03Temp : "7°C",
-  day04Name : "Thursday",
-  day04Temp : "10°C",
-  day05Name : "Friday",
-  day05Temp : "13°C"
-};
 // Component to display future temperatures
 function FutureTempsBar(){
-    return(<>
-    <div className={styles.FutureTempsBarContainer}>
-        <TempForFutureDay day={weatherData.day01Name} temperature={weatherData.day01Temp} className={styles.FutureTemp}/>
-        <TempForFutureDay day={weatherData.day02Name} temperature={weatherData.day02Temp} className={styles.FutureTemp}/>
-        <TempForFutureDay day={weatherData.day03Name} temperature={weatherData.day03Temp} className={styles.FutureTemp}/>
-        <TempForFutureDay day={weatherData.day04Name} temperature={weatherData.day04Temp} className={styles.FutureTemp}/>
-    </div></>)
+    return(<></>)
 }
 
 export default FutureTempsBar;
--- src/Components/TempsForFutureDays/TempForFutureDay.js
+++ src/Components/TempsForFutureDays/TempForFutureDay.js
@@ -1,12 +1,14 @@
+import styles from "./FutureTempsBar.module.css"
+
 // This displays the temperature for the future days by taking the day and temperature as props
 function TempForFutureDay(props) {
     return(
-        <div>
+        <div className={props.c}>
             <h2>{props.day}</h2>
             <figure>
                 <img src="..../public/images/weather-icon.png" alt="weather_icon"/>
             </figure>
-            <h2>{props.temperature}</h2>
+            <h2>{props.temp}°C</h2>
         </div>
     )
 }
